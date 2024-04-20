a5ba98f7fa7e1a1dcafc67d0f578bd9fa8534cb6
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2024-04-20 13:35:00 +0800

    Update 厂长.py

--- py/厂长.py
+++ py/厂长.py
@@ -20,7 +20,7 @@ class Spider(Spider):  # 元类 默认的元类 type
     def homeContent(self, filter):
         result = {}
         cateManual = {
-           
+            "电影": "dy",
             "电视剧": "dsj",
             "国产剧": "gcj",
             "美剧": "meijutt",
