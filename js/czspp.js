f3c80430e815144e54fac7ac85daf088f28edd0f
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2024-08-09 14:45:14 +0800

    Update czspp.js

--- js/czspp.js
+++ js/czspp.js
@@ -2,7 +2,7 @@ var rule={
     title:'厂长资源',
     // host:'https://czspp.com', 
     // host:'https://czz9.com', 
-    host:'https://cz01.vip/',
+    host:'http://www.czys.pro/',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = jsp.pdfh(html,"h3&&a&&href")',
     url:'/fyclassfyfilter',
     filterable:1,//是否启用分类筛选,
