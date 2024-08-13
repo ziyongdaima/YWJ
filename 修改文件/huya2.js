9eb26ca8ca0e0a7350f47e1df415c792c2e9bbe2
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2024-08-13 23:35:09 +0800

    Update huya2.js

--- 修改文件/huya2.js
+++ 修改文件/huya2.js
@@ -3,8 +3,8 @@ var rule = {
     host:'https://www.huya.com',
     homeUrl:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=2168&tagAll=0&page=1',//用于"分类获取"和"推荐获取"
     url:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=fyfilter&tagAll=0&page=fypage',
-    class_name:'娱乐&网游&单机&手游&电影',
-    class_url:'8&1&2&3&2067',
+    class_name:'娱乐&网游&单机&手游',
+    class_url:'8&1&2&3',
     // detailUrl:'https://m.huya.com/fyid',//二级详情拼接链接(json格式用)
     detailUrl:'http://live.yj1211.work/api/live/getRoomInfo?uid=&platform=huya&roomId=fyid',//二级详情拼接链接(json格式用)
     filterable: 1,
