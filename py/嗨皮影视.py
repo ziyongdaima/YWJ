dd8ed640da76fa2fd6409794728dd06989f81fed
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2025-05-27 23:03:51 +0800

    Add files via upload

--- py/嗨皮影视.py
+++ py/嗨皮影视.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 作者：@嗷呜
+# by @嗷呜
 import sys
 sys.path.append('..')
 from base.spider import Spider
@@ -8,7 +8,7 @@ import requests
 
 class Spider(Spider):
 
-    def __init__(self, extend=""):
+    def init(self, extend=""):
         pass
 
     def getName(self):
@@ -27,6 +27,7 @@ class Spider(Spider):
         result = {}
         cateManual = {
             # "直播": "live",
+            '排行榜': 'rank',
             "电影": "1",
             "剧集": "2",
             "综艺": "3",
@@ -44,7 +45,7 @@ class Spider(Spider):
 
     host = "https://wys.upfuhn.com"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " 
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/80.0.3987.149 Safari/537.36"
     }
 
@@ -61,31 +62,31 @@ class Spider(Spider):
         return videos
 
     def homeVideoContent(self):
-        url = f'"{self.host}/v1/ys_video_sites/hot?t=1"
+        url = f'{self.host}/v1/ys_video_sites/hot?t=1'
         data = requests.get(url, headers=self.headers).json()
         videos = self.list(data['data']['data'])
         result = {'list': videos}
-        返回结果
+        return result
 
     def categoryContent(self, tid, pg, filter, extend):
-        path = f' /v1/ys_video_sites?t={tid}&s_t=0&a&y&o=0&ps=21&pn={pg}'
+        path = f'/v1/ys_video_sites?t={tid}&s_t=0&a&y&o=0&ps=21&pn={pg}'
         rank = False
-        if tid == 'rank>:
+        if tid == 'rank':
             if pg == 1:
-                path = f' /v1/ys_video_sites/ranking'
+                path = f'/v1/ys_video_sites/ranking'
                 rank = True
             else:
                 path = ''
         # elif tid == 'live' and pg == 1:
-        #     path = f' /v1/ys_live_tvs'
+        #     path = f'/v1/ys_live_tvs'
         videos = []
         result = {}
         try:
             data = requests.get(self.host + path, headers=self.headers).json()
             if rank:
                 for video in data['data']:
-                    videos.
-            else:extend(data['data'][video])
+                    videos.extend(data['data'][video])
+            else:
                 videos = data['data']['data']
             result = {}
             result['list'] = self.list(videos)
@@ -101,29 +102,29 @@ class Spider(Spider):
         tid = ids[0]
         url = f'{self.host}/v1/ys_video_series/by_vid/{tid}'
         data = requests.get(url, headers=self.headers).json()
-        data1 = data['data']['ys_video_site']]
+        data1 = data['data']['ys_video_site']
         urls = []
         for it in data['data']['data']:
-            urls.append(it['系列编号'] + '$' + it['视频链接'])
+            urls.append(it['series_num'] + '$' + it['video_url'])
         vod = {
-            '视频名称': data1['视频名称'],
-            '类型名称': data1['标签'],
-            '视频年份': data1['年份'],
-            '视频地区': data1['地区'],
-            '视频导演': data1['主演'],
-            '视频内容': data1['视频描述'],
-            '视频播放来源': '嗨皮在线',
-            '视频播放链接': '#'.join(urls),
+            'vod_name': data1['video_name'],
+            'type_name': data1['tag'],
+            'vod_year': data1['years'],
+            'vod_area': data1['area'],
+            'vod_director': data1['main_actor'],
+            'vod_content': data1['video_desc'],
+            'vod_play_from': '嗨皮在线',
+            'vod_play_url': '#'.join(urls),
         }
         result = {
-            '列表':[
+            'list': [
                 vod
             ]
         }
         return result
 
     def searchContent(self, key, quick, pg=1):
-        url = f'/{self.host}/v1/ys_video_sites/search?s={key}&o=0&ps=200&pn={pg}'
+        url = f'{self.host}/v1/ys_video_sites/search?s={key}&o=0&ps=200&pn={pg}'
         data = requests.get(url, headers=self.headers).json()
         videos = data['data']['video_sites']
         if data['data']['first_video_series'] is not None:
@@ -137,9 +138,9 @@ class Spider(Spider):
         result = {
             'url': id,
             'parse': 0,
-            'header': self.
-        返回结果
-        }headers
+            'header': self.headers
+        }
+        return result
 
-    def localProxy(self, param>:
+    def localProxy(self, param):
         pass
